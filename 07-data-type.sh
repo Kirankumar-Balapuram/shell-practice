0e0989302c0b34f7efdfa4bcff93860fcced64a0
kiran <bkiran1407@gmail.com>
2026-02-20 13:15:32 +0530

    Fix variable case for fruits array

--- 07-data-type.sh
+++ 07-data-type.sh
@@ -8,9 +8,9 @@ SUM=$(($NUM1+$NUM2))
 echo "Sum is: $SUM"
 
 # Array
-Fruits=("Apple" "Banana" "Pomo")
+FRUITS=("Apple" "Banana" "Pomo")
 
-echo "Fruits are: ${fruits[@]}"
-echo "First Fruits is: ${fruits[0]}"
-echo "Second Fruits is: ${fruits[1]}"
-echo "Third Fruits is: ${fruits[2]}"
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruits is: ${FRUITS[0]}"
+echo "Second Fruits is: ${FRUITS[1]}"
+echo "Third Fruits is: ${FRUITS[2]}"
