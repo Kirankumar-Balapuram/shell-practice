768af2410b8252b946ddb760df4c50b1e0f97eb2
kiran <bkiran1407@gmail.com>
2026-03-01 08:23:52 +0530

    Create 22-backup.sh

---
+++ 22-backup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is default value, if the user is not give values
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access"
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days ] $N"
+    exit  1
+}
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi     
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R source Directory $SOURCE_DIR does not exist"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R Destination Directory $DEST_DIR does not exist"
+    exit 1
+fi
+
+### Find the files
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
